088b593f7ef8f44e5ca9c912812baa4f50e5fe20
Sergei Zakharenko <Sergei.Rabota@gmail.com>
2023-10-02 22:35:34 +0300

    DB Tables were filled with initial data (excl. user, car, post, image)

---
+++ db/scripts/106_dml_insert_transmissions.sql
@@ -0,0 +1,5 @@
+INSERT INTO transmission(name) VALUES
+('Механика'),
+('Автомат'),
+('Робот'),
+('Вариатор');
---
+++ db/scripts/108_dml_insert_categories.sql
@@ -0,0 +1,6 @@
+INSERT INTO category(name) VALUES
+('A'),
+('B'),
+('B1'),
+('C'),
+('D');
